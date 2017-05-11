6f31adc56d2849f30fbce848e653852144121a28
Renan Bastos <renanbastos.tec@gmail.com>
2017-05-11 02:58:26 -0400

    [+] service http to consume routes of API

---
+++ sources/_js/service.js
@@ -0,0 +1,73 @@
+var baseUrl = 'https://astronautjs.alexslx.xyz/api/';
+
+/*
+ * Routes
+ * category, place, account
+ */
+var http = {};
+
+/*
+ * Method GET
+ * http.get('Routes', callback);
+ */
+http.get = function(url, callback){
+  $.ajax({
+    url: baseUrl+url,
+    success: function(response) {
+      callback({status: 'success', data: response});
+    },
+    error: function(response){
+      callback({status: 'error', data: response});
+    }
+  });
+};
+
+/*
+ * Method POST
+ * http.post('Routes', {name: 'newName', picture:'image.svg'}, callback);
+ */
+http.post = function(url, data, callback){
+  $.post(baseUrl+url,
+      data,
+      function(response, status){
+          callback({status: status, data: response});
+      });
+};
+
+/*
+ * Method PUT
+ * http.put('Routes', {name: 'yourName'}, callback);
+ */
+http.put = function(url, data, callback){
+  $.ajax({
+        url: baseUrl+url,
+        type: 'PUT',
+        data: data,
+        dataType: 'json',
+        success: function(response){
+          callback({status: 'success', data: response});
+        },
+        error: function(response){
+          callback({status: 'error', data: response});
+        }
+  });
+};
+
+/*
+ * Method DELETE
+ * http.delete('Routes', {_id: 'yourId'}, callback);
+ */
+http.delete = function(url, data, callback){
+  $.ajax({
+        url: baseUrl+url,
+        type: 'DELETE',
+        data: data,
+        dataType: 'json',
+        success: function(response){
+          callback({status: 'success', data: response});
+        },
+        error: function(response){
+          callback({status: 'error', data: response});
+        }
+  });
+};

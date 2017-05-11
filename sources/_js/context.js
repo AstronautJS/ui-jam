3dad4aea9654e66f806f29ebe60cf8b1b5fa73bb
Gabriel Corrêa <gbl.correa8@gmail.com>
2017-05-11 02:01:51 -0300

    [+] navbar

--- sources/_js/context.js
+++ sources/_js/context.js
@@ -2,11 +2,15 @@
     'use strict';
 
     var DOM = {
-        footer: document.getElementById('footer')    
+        footer: document.getElementById('footer'),
+        navbar: document.getElementById('navbar')
     };
     var RENDERED = {};
 
-    
+
     RENDERED.barAction = nunjucks.render('_templates/bar-action.tpl.html', {});
+    RENDERED.navAction = nunjucks.render('_templates/navbar.tpl.html', {});
+
     DOM.footer.innerHTML = RENDERED.barAction;
+    DOM.navbar.innerHTML = RENDERED.navAction;
 })();
